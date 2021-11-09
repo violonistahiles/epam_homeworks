dac9300dbb9c025c88be43425acc44ecf9a421d0
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-11-10 00:02:03 +0300

    Warming up: Change folders structure

--- tests/homework1/calculator_tests/test_calculator.py
+++ tests/homework1/calculator_tests/test_calculator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from calculator.calc import check_power_of_2
+from homework1.sample_project.calculator.calc import check_power_of_2
 
 def test_positive_case():
     """Testing that actual powers of 2 give True"""
