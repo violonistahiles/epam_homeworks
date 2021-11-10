aae439e17f7d7289945a410e862fd0e23ba7814f
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-11-10 20:39:50 +0300

    Task03: Add algorithm

--- homework1/task03.py
+++ homework1/task03.py
@@ -1,6 +1,6 @@
 """
-Write down the function, which reads input line-by-line, and find maximum and minimum values.
-Function should return a tuple with the max and min values.
+Write down the function, which reads input line-by-line, and find maximum and
+minimum values. Function should return a tuple with the max and min values.
 
 For example for [1, 2, 3, 4, 5], function should return [1, 5]
 
@@ -17,4 +17,24 @@ from typing import Tuple
 
 
 def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
-    ...
+
+    with open(file_name) as file_example:
+        # Get first line as initial value
+        start_number_str = file_example.readline().strip()
+        min_value = int(start_number_str)
+        max_value = min_value
+        # Process other lines and compare values
+        for line in file_example:
+            number = int(line.strip())
+
+            if number < min_value:
+                min_value = number
+            if number > max_value:
+                max_value = number
+
+    return min_value, max_value
+
+
+test_path = 'C://Users/kurushin/Desktop/NN/python_practice/epam/hm1_task3.txt'
+result = find_maximum_and_minimum(test_path)
+print(result)
---
+++ tests/homework1/test_task03.py
@@ -0,0 +1,16 @@
+import io
+from homework1.task03 import find_maximum_and_minimum
+
+
+def test_positive_case():
+    """
+    Testing that from text file minimum
+    and maximum value returns correctly
+    """
+    # some_string = "1\n2\n3\n4"
+    # file_dummy = io.StringIO("1\n2\n3\n4")
+    # right_value = (1, 4)
+    #
+    # test_result = find_maximum_and_minimum(some_string)
+    #
+    # assert test_result == right_value
