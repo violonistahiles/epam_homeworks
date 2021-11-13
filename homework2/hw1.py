093773e98aff9a676e1a4fec02613fe36e1b4fe9
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-11-13 20:23:38 +0300

    hw1: Add algorithms for tasks and its tests

--- homework2/hw1.py
+++ homework2/hw1.py
@@ -6,24 +6,125 @@ Given a file containing text. Complete using only default collections:
     4) Count every non ascii char
     5) Find most common non ascii char for document
 """
-from typing import List
+from typing import List, Set, Union
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
-    ...
+    """
+    Find in text 10 longest words consisting
+    from largest amount of unique symbols
+    """
+    symbols_to_replace = '!"#$%&()*+,./:;<=>?@[\\]^_{|}~»«'
+    words_set = set()
+
+    def process_line(line: str, words_set: Set) -> None:
+        """Delete useless symbols from string and get info about words"""
+        for symbol in symbols_to_replace:
+            line = line.replace(symbol, '')
+
+        words_set_temp = set([word.strip('\t ') for word in line.split()])
+        for word in words_set_temp:
+            words_set.add((len(set(word)), len(word), word))
+
+        return None
+
+    # Process file line by line
+    with open(file_path, 'rb') as text:
+        line = text.readline().decode('unicode-escape')
+        while line != '':
+            line = line.strip('\r\n')
+            while line.endswith('-'):  # Concatenate lines with word wrap
+                line = line[:-1] + text.readline().decode('unicode-escape')
+                line = line.strip('\r\n')
+            process_line(line, words_set)
+            line = text.readline().decode('unicode-escape')
+
+    # Sort tuples: first by largest amount of unique symbols
+    # second by word length
+    sorted_words = sorted(words_set, reverse=True)
+    final_words = [word_params[2] for word_params in sorted_words]
+
+    if len(final_words) < 10:
+        return final_words
+    else:
+        return final_words[:10]
 
 
 def get_rarest_char(file_path: str) -> str:
-    ...
+    """Find rarest symbol for document"""
+    symbols_dict = dict()
+
+    with open(file_path, 'rb') as file_example:
+        line = file_example.readline().decode('unicode-escape')
+        while line != '':
+            for symbol in line:
+                if symbol in symbols_dict:
+                    symbols_dict[symbol] += 1
+                else:
+                    symbols_dict[symbol] = 1
+
+            line = file_example.readline().decode('unicode-escape')
+
+    min_count = min(symbols_dict.values())
+    rare_chars = filter(lambda x: x[1] == min_count, symbols_dict.items())
+    rarest_char = sorted(rare_chars)[-1][0]
+
+    return rarest_char
 
 
 def count_punctuation_chars(file_path: str) -> int:
-    ...
+    """Count every punctuation char in document"""
+    punctuation_number = 0
+    punctuation_chars = '!",.-:;?\'()—…'
+
+    with open(file_path, 'rb') as file_example:
+        line = file_example.readline().decode('unicode-escape')
+        while line != '':
+            for symbol in line:
+                if symbol in punctuation_chars:
+                    punctuation_number += 1
+
+            line = file_example.readline().decode('unicode-escape')
+
+    return punctuation_number
 
 
 def count_non_ascii_chars(file_path: str) -> int:
-    ...
+    """Count every non ascii char in document"""
+    non_ascii_number = 0
+
+    with open(file_path, 'rb') as file_example:
+        line = file_example.readline().decode('unicode-escape')
+        while line != '':
+            for symbol in line:
+                if not symbol.isascii():
+                    non_ascii_number += 1
+
+            line = file_example.readline().decode('unicode-escape')
+
+    return non_ascii_number
 
 
-def get_most_common_non_ascii_char(file_path: str) -> str:
-    ...
+def get_most_common_non_ascii_char(file_path: str) -> Union[str, None]:
+    """Find most common non ascii char for document"""
+    non_ascii = dict()
+
+    with open(file_path, 'rb') as file_example:
+        line = file_example.readline().decode('unicode-escape')
+        while line != '':
+            for symbol in line:
+                if not symbol.isascii():
+                    if symbol in non_ascii:
+                        non_ascii[symbol] += 1
+                    else:
+                        non_ascii[symbol] = 1
+
+            line = file_example.readline().decode('unicode-escape')
+
+    if len(non_ascii) == 0:
+        return None
+    else:
+        max_count = max(non_ascii.values())
+        common_chars = filter(lambda x: x[1] == max_count, non_ascii.items())
+        common_char = sorted(common_chars)[-1][0]
+        return common_char
---
+++ tests/homework2/test_hw1.py
@@ -0,0 +1,156 @@
+from unittest import mock
+
+from homework2.hw1 import *
+
+
+def test_longest_diverse_words_when_words_less_then_ten():
+    """Testing algorithm when there are less then ten words in document"""
+    dummy_path = 'some_path'
+    text_line_1 = 'abcdef abc acbdabc abcdcd\n'
+    text_line_2 = 'abcdefg aaaa\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = ['abcdefg', 'abcdef', 'acbdabc', 'abcdcd', 'abc', 'aaaa']
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = get_longest_diverse_words(dummy_path)
+
+    assert test_result == correct_result
+
+
+def test_longest_diverse_words_when_words_more_then_ten():
+    """Testing algorithm when there are more then ten words in document"""
+    dummy_path = 'some_path'
+    text_line_1 = 'abc a ab abcd abb aff\n'
+    text_line_2 = 'xyz x xy xyzu xyy xll\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = ['xyzu', 'abcd', 'xyz', 'abc', 'xyy',
+                      'xll', 'aff', 'abb', 'xy', 'ab']
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = get_longest_diverse_words(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def test_rarest_char_case():
+    """Testing that algorithm works fine on standard data"""
+    dummy_path = 'some_path'
+    text_line_1 = 'fmdo gis pos a fposd\n'
+    text_line_2 = 'fd smj fsdi fmois fjoi z\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = 'z'
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = get_rarest_char(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def test_count_punctuation_chars_case():
+    """Testing that algorithm works fine on standard data"""
+    dummy_path = 'some_path'
+    text_line_1 = 'fmdo, gis! pos. a- fposd\n'
+    text_line_2 = 'fd smj fsdi fm(ois) fjoi z"\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = 7
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = count_punctuation_chars(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def test_count_punctuation_chars_when_punctuation_chars_not_exist():
+    """Testing algorithm when there are no punctuation chars in document"""
+    dummy_path = 'some_path'
+    text_line_1 = 'fmdo gis pos a fposd\n'
+    text_line_2 = 'fd smj fsdi fmois fjoi z\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = 0
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = count_punctuation_chars(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def test_count_non_ascii_chars_case():
+    """Testing that algorithm works fine on standard data"""
+    dummy_path = 'some_path'
+    text_line_1 = 'fmdo, gis! pos. a\u00f6- fposd\n'
+    text_line_2 = 'fd smj f\u00dfsdi fm(ois) \u00e4fjoi z"\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = 3
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = count_non_ascii_chars(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def test_count_non_ascii_chars_when_non_ascii_not_exist():
+    """Testing algorithm when there are no non ascii symbols in document"""
+    dummy_path = 'some_path'
+    text_line_1 = 'fmdo, gis! pos. a- fposd\n'
+    text_line_2 = 'fd smj fsdi fm(ois) fjoi z"\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = 0
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = count_non_ascii_chars(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def get_most_common_non_ascii_char():
+    """Testing that algorithm works fine on standard data"""
+    dummy_path = 'some_path'
+    text_line_1 = 'f\u00e4mdo, g\u00f6is! pos. a\u00f6- fposd\n'
+    text_line_2 = 'fd smj f\u00dfsdi fm(ois) \u00e4fjo\u00f6i z"\n'
+    test_text = text_line_1 + text_line_2
+    correct_result = '\u00f6'
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = get_most_common_non_ascii_char(dummy_path)
+    print(test_result)
+
+    assert test_result == correct_result
+
+
+def get_most_common_non_ascii_when_non_ascii_not_exist():
+    """Testing algorithm when there are no non ascii symbols in document"""
+    dummy_path = 'some_path'
+    text_line_1 = 'fmdo, gis! pos. a- fposd\n'
+    text_line_2 = 'fd smj fsdi fm(ois) fjoi z"\n'
+    test_text = text_line_1 + text_line_2
+
+    mock_open = mock.mock_open(read_data=test_text.encode('unicode-escape'))
+
+    with mock.patch('homework2.hw1.open', mock_open):
+        test_result = get_most_common_non_ascii_char(dummy_path)
+    print(test_result)
+
+    assert test_result is False
+
