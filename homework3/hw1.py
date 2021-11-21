44b855df302be4169ee5705e1418a76e53bb2bc0
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-11-21 18:10:06 +0300

    Task1: Make function more readable

--- homework3/hw1.py
+++ homework3/hw1.py
@@ -6,54 +6,61 @@ and give it out times number only.
 def f():
     return input('? ')
 
->>> f()
-? 1
-'1'
->>> f()      # will remember previous value
-'1'
->>> f()      # but use it up to two times only
-'1'
->>> f()
-? 2
-'2'
+# >>> f()
+# ? 1
+# '1'
+# >>> f()      # will remember previous value
+# '1'
+# >>> f()      # but use it up to two times only
+# '1'
+# >>> f()
+# ? 2
+# '2'
 """
 from typing import Any, Callable
 
 
 class CacheSizeError(Exception):
-    def __init__(self):
+    def __init__(self) -> None:
         print('Argument "times" should be int and greater then 0')
+
+
+class Cache:
+    def __init__(self) -> None:
+        self.value = 0
+        self.size = 0
 
 
 def cache(times: int) -> Callable:
     """Cache function result for n invokes"""
-
     if not isinstance(times, int) or times <= 0:
         raise CacheSizeError()
-
-    cache_value = []
-    cache_size = []
+    # Create class for caching results
+    memory = Cache()
 
     def wrapper(func: Callable) -> Callable:
         def inner_wrapper(*args) -> Any:
-            if len(cache_size) > 0 and cache_size[0] >= 1:
-                cache_size[0] -= 1
-                return cache_value[0]
+            if memory.size > 0:
+                memory.size -= 1
+                return memory.value
             else:
                 result = func(*args)
 
-                if len(cache_value) == 0:
-                    cache_value.append(result)
-                else:
-                    cache_value[0] = result
-
-                if len(cache_size) == 0:
-                    cache_size.append(times)
-                else:
-                    cache_size[0] = times
+                memory.value = result
+                memory.size = times
 
                 return result
 
         return inner_wrapper
 
     return wrapper
+
+
+@cache(times=1)
+def function_for_test():
+    return input('?')
+
+
+if __name__ == '__main__':
+    for i in range(2):
+        print(function_for_test())
