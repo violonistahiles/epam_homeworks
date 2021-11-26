7d916dbe3d8c9388efafe632c93fcbdf6dfe32f2
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-11-27 00:06:16 +0300

    Task01: Fixed remarks and updated accordingly to practice

--- homework2/hw1.py
+++ homework2/hw1.py
@@ -6,38 +6,57 @@ Given a file containing text. Complete using only default collections:
     4) Count every non ascii char
     5) Find most common non ascii char for document
 """
-from typing import List, Set, Union
+import unicodedata
+from collections import defaultdict
+from typing import Dict, Iterator, List, Set, Union
 
 
-def get_longest_diverse_words(file_path: str) -> List[str]:
+def lines_generator(
+        file_path: str, encoding: str = 'utf-8', errors: str = 'ignore'
+) -> Iterator[str]:
+    """Yield lines from text file"""
+    with open(file_path, encoding=encoding, errors=errors) as text:
+        line = text.readline()
+        while line:
+            while line.endswith('-\r\n') or line.endswith('-\n'):
+                line = line[:line.rfind('-')] + text.readline()
+            yield line
+            line = text.readline()
+
+
+def tokenize_line(line: str) -> Set[str]:
+    """Return connected sequences of unicode word symbols"""
+    word = ''
+    words_set = set()
+    for symbol in line:
+        if unicodedata.category(symbol).startswith('L'):
+            word += symbol
+        else:
+            if word:
+                words_set.add(word)
+            word = ''
+
+    if word:
+        words_set.add(word)
+
+    return words_set
+
+
+def get_longest_diverse_words(
+        file_path: str, encoding: str = 'utf-8', errors: str = 'ignore'
+) -> List[str]:
     """
     Find in text 10 longest words consisting
     from largest amount of unique symbols
     """
-    symbols_to_replace = '!"#$%&()*+,./:;<=>?@[\\]^_{|}~»«'
     words_set = set()
-
-    def process_line(line: str, words_set: Set) -> None:
-        """Delete useless symbols from string and get info about words"""
-        for symbol in symbols_to_replace:
-            line = line.replace(symbol, '')
-
-        words_set_temp = set([word.strip('\t ') for word in line.split()])
-        for word in words_set_temp:
-            words_set.add((len(set(word)), len(word), word))
-
-        return None
+    line_gen = lines_generator(file_path, encoding, errors)
 
     # Process file line by line
-    with open(file_path, 'rb') as text:
-        line = text.readline().decode('unicode-escape')
-        while line != '':
-            line = line.strip('\r\n')
-            while line.endswith('-'):  # Concatenate lines with word wrap
-                line = line[:-1] + text.readline().decode('unicode-escape')
-                line = line.strip('\r\n')
-            process_line(line, words_set)
-            line = text.readline().decode('unicode-escape')
+    for line in line_gen:
+        tmp_words_set = tokenize_line(line)
+        for word in tmp_words_set:
+            words_set.add((len(set(word)), len(word), word))
 
     # Sort tuples: first by largest amount of unique symbols
     # second by word length
@@ -50,20 +69,18 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
         return final_words[:10]
 
 
-def get_rarest_char(file_path: str) -> str:
+def get_rarest_char(
+    file_path: str, encoding: str = 'utf-8', errors: str = 'ignore'
+) -> str:
     """Find rarest symbol for document"""
-    symbols_dict = dict()
+    symbols_dict = defaultdict(int)
 
-    with open(file_path, 'rb') as file_example:
-        line = file_example.readline().decode('unicode-escape')
-        while line != '':
-            for symbol in line:
-                if symbol in symbols_dict:
-                    symbols_dict[symbol] += 1
-                else:
-                    symbols_dict[symbol] = 1
+    line_gen = lines_generator(file_path, encoding, errors)
 
-            line = file_example.readline().decode('unicode-escape')
+    # Process file line by line
+    for line in line_gen:
+        for symbol in line:
+            symbols_dict[symbol] += 1
 
     min_count = min(symbols_dict.values())
     rare_chars = filter(lambda x: x[1] == min_count, symbols_dict.items())
@@ -72,59 +89,63 @@ def get_rarest_char(file_path: str) -> str:
     return rarest_char
 
 
-def count_punctuation_chars(file_path: str) -> int:
+def count_punctuation_chars(
+    file_path: str, encoding: str = 'utf-8', errors: str = 'ignore'
+) -> int:
     """Count every punctuation char in document"""
     punctuation_number = 0
-    punctuation_chars = '!",.-:;?\'()—…'
 
-    with open(file_path, 'rb') as file_example:
-        line = file_example.readline().decode('unicode-escape')
-        while line != '':
-            for symbol in line:
-                if symbol in punctuation_chars:
-                    punctuation_number += 1
+    line_gen = lines_generator(file_path, encoding, errors)
 
-            line = file_example.readline().decode('unicode-escape')
+    # Process file line by line
+    for line in line_gen:
+        for symbol in line:
+            if unicodedata.category(symbol).startswith('P'):
+                punctuation_number += 1
 
     return punctuation_number
 
 
-def count_non_ascii_chars(file_path: str) -> int:
+def count_non_ascii_chars(
+    file_path: str, encoding: str = 'utf-8', errors: str = 'ignore'
+) -> int:
     """Count every non ascii char in document"""
     non_ascii_number = 0
 
-    with open(file_path, 'rb') as file_example:
-        line = file_example.readline().decode('unicode-escape')
-        while line != '':
-            for symbol in line:
-                if not symbol.isascii():
-                    non_ascii_number += 1
+    line_gen = lines_generator(file_path, encoding, errors)
 
-            line = file_example.readline().decode('unicode-escape')
+    # Process file line by line
+    for line in line_gen:
+        for symbol in line:
+            if not symbol.isascii():
+                non_ascii_number += 1
 
     return non_ascii_number
 
 
-def get_most_common_non_ascii_char(file_path: str) -> Union[str, None]:
+def get_most_common_non_ascii_char(
+    file_path: str, encoding: str = 'utf-8', errors: str = 'ignore'
+) -> Union[str, None]:
     """Find most common non ascii char for document"""
-    non_ascii = dict()
 
-    with open(file_path, 'rb') as file_example:
-        line = file_example.readline().decode('unicode-escape')
-        while line != '':
-            for symbol in line:
-                if not symbol.isascii():
-                    if symbol in non_ascii:
-                        non_ascii[symbol] += 1
-                    else:
-                        non_ascii[symbol] = 1
+    def get_most_common(input_dict: Dict) -> str:
+        """Return most common element from dict"""
+        max_count = max(input_dict.values())
+        common_chars = filter(lambda x: x[1] == max_count, input_dict.items())
+        common_char = sorted(common_chars)[-1][0]
+        return common_char
 
-            line = file_example.readline().decode('unicode-escape')
+    non_ascii = defaultdict(int)
+
+    line_gen = lines_generator(file_path, encoding, errors)
+
+    # Process file line by line
+    for line in line_gen:
+        for symbol in line:
+            if not symbol.isascii():
+                non_ascii[symbol] += 1
 
     if len(non_ascii) == 0:
         return None
     else:
-        max_count = max(non_ascii.values())
-        common_chars = filter(lambda x: x[1] == max_count, non_ascii.items())
-        common_char = sorted(common_chars)[-1][0]
-        return common_char
+        return get_most_common(non_ascii)
