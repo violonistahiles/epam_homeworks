467190c56e299020866a92473ec21f5730afd748
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-12-05 22:34:01 +0300

    Task01: Add tests

--- homework7/hw1.py
+++ homework7/hw1.py
@@ -27,8 +27,65 @@ example_tree = {
 }
 
 
+types_to_process = (int, bool, str)
+all_types = (str, list, tuple, dict, set, int, bool)
+
+
+class NotDictError(Exception):
+    """Input tree must be dict"""
+
+
+def compare_element(value: Any, element: Any) -> int:
+    """Compare value and element"""
+    if isinstance(element, all_types):
+        return int(value == element)
+
+    raise ValueError(f'Unsupported data type {type(value)}')
+
+
+def process_branch(branch: Any, element: Any) -> int:
+    """Step recursively through the branch"""
+    # Check if branch is int, str or bool to stop recursion
+    if isinstance(branch, types_to_process):
+        result = compare_element(branch, element)
+        return result
+
+    result = 0
+    for value in branch:
+        result += process_value(value, element)
+
+    return result
+
+
+def process_value(value: Any, element: Any) -> int:
+    """
+    Check if value can be compared with element,
+    else go deeper in value structure
+    """
+    result = 0
+    if isinstance(value, type(element)):
+        result = compare_element(value, element)
+        if result:
+            return result
+
+    # If dict -> iterate by its values
+    if isinstance(value, dict):
+        value = value.values()
+    result += process_branch(value, element)
+
+    return result
+
+
 def find_occurrences(tree: dict, element: Any) -> int:
-    ...
+    """Count the number of element occurrences in tree"""
+
+    if not isinstance(tree, dict):
+        raise NotDictError
+
+    result = 0
+    for value in tree.values():
+        result += process_value(value, element)
+    return result
 
 
 if __name__ == '__main__':
---
+++ tests/homework7/test_hw1.py
@@ -0,0 +1,177 @@
+from unittest.mock import Mock, patch
+
+import pytest
+
+from homework7.hw1 import (NotDictError, compare_element, find_occurrences,
+                           process_branch, process_value)
+
+
+def test_compare_element_int():
+    """Testing compare_element works correct with int data type"""
+    element = 1
+    element_to_compare = 1
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_str():
+    """Testing compare_element works correct with str data type"""
+    element = 'a'
+    element_to_compare = 'a'
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_bool():
+    """Testing compare_element works correct with bool data type"""
+    element = True
+    element_to_compare = True
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_set():
+    """Testing compare_element works correct with set data type"""
+    element = {1, 4, 5}
+    element_to_compare = {1, 4, 5}
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_list():
+    """Testing compare_element works correct with list data type"""
+    element = [1, 4, 5]
+    element_to_compare = [1, 4, 5]
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_tuple():
+    """Testing compare_element works correct with tuple data type"""
+    element = (1, 4, 5)
+    element_to_compare = (1, 4, 5)
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_dict():
+    """Testing compare_element works correct with dict data type"""
+    element = {1: 2, 'a': 'b'}
+    element_to_compare = {1: 2, 'a': 'b'}
+
+    assert compare_element(element, element_to_compare)
+
+
+def test_compare_element_wrong_data_type():
+    """Testing compare_element raise error with unsupported data type"""
+    element = {1: 2, 'a': 'b'}
+    element_to_compare = object()
+
+    with pytest.raises(ValueError):
+        compare_element(element, element_to_compare)
+
+
+def test_compare_element_different_values():
+    """Testing compare_element return 0 with different values"""
+    element = {1: 2, 'a': 'b'}
+    element_to_compare = 25
+
+    assert not compare_element(element, element_to_compare)
+
+
+def test_process_value_elements_with_equal_data_type():
+    """
+    Testing process_value invoke compare_element only once with
+    equal value and element
+    """
+    element = {1: 2, 'a': 'b'}
+    element_to_compare = {1: 2, 'a': 'b'}
+    mock = Mock(return_value=1)
+
+    with patch('homework7.hw1.compare_element', mock) as mock_ce:
+        assert process_value(element, element_to_compare)
+        mock_ce.assert_called_once()
+
+
+def test_process_value_elements_with_different_data_type():
+    """
+    Testing process_value invoke process_branch only once with
+    different value and element
+    """
+    element = {1: 2, 'a': 'b'}
+    element_to_compare = 12
+    mock = Mock(return_value=1)
+
+    with patch('homework7.hw1.process_branch', mock) as mock_pb:
+        assert process_value(element, element_to_compare)
+        mock_pb.assert_called_once()
+
+
+def test_process_value_elements_with_equal_data_type_but_different_value():
+    """
+    Testing process_value with equal input data type but different value
+    invoke process_branch only and compare_element once
+    """
+    element = {1: 2, 'a': 'b'}
+    element_to_compare = {1: 2, 'a': 'c'}
+    first_mock = Mock(return_value=0)
+    second_mock = Mock(return_value=0)
+
+    with patch('homework7.hw1.compare_element', first_mock) as mock_ce, \
+            patch('homework7.hw1.process_branch', second_mock) as mock_pb:
+        assert not process_value(element, element_to_compare)
+        mock_ce.assert_called_once()
+        mock_pb.assert_called_once()
+
+
+def test_find_occurrences_input_tree_dict():
+    """
+    Testing find_occurrences invoke
+    process_value times equal to tree length
+    """
+    test_tree = {1: 2, 'a': 'b', (1, 'a'): 'c'}
+    element_to_compare = 25
+    correct_result = len(test_tree)
+    mock = Mock(return_value=1)
+
+    with patch('homework7.hw1.process_value', mock):
+        test_result = find_occurrences(test_tree, element_to_compare)
+        assert test_result == correct_result
+
+
+def test_find_occurrences_input_tree_not_dict():
+    """Testing find_occurrences raise error when tree is not a dict"""
+    test_tree = [1, 2, 3]
+    element_to_compare = 25
+
+    with pytest.raises(NotDictError):
+        _ = find_occurrences(test_tree, element_to_compare)
+
+
+def test_process_branch_with_iterable_input():
+    """
+    Testing process_branch invoke process_value
+    times equal to branch length
+    """
+    test_tree = [1, 2, 'ads']
+    element_to_compare = 25
+    correct_result = len(test_tree)
+    mock = Mock(return_value=1)
+
+    with patch('homework7.hw1.process_value', mock):
+        test_result = process_branch(test_tree, element_to_compare)
+        assert test_result == correct_result
+
+
+def test_process_branch_with_non_iterable_input():
+    """
+    Testing process_branch invoke process_value
+    times equal to branch length
+    """
+    test_tree = 5
+    element_to_compare = 25
+    mock = Mock(return_value=1)
+
+    with patch('homework7.hw1.compare_element', mock) as mock_ce:
+        assert process_branch(test_tree, element_to_compare)
+        mock_ce.assert_called_once()
