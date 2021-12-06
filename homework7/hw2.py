6ed5ce48e19e13c8879ad253440ea43839901fb1
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-12-06 22:57:44 +0300

    Add algorithms for task02 and task03

--- homework7/hw2.py
+++ homework7/hw2.py
@@ -20,5 +20,49 @@ Examples:
 """
 
 
+def process_string(string: str) -> str:
+
+    cut_last = string.endswith('#')
+
+    split_string = string.split('#')
+    print(len(split_string))
+    if len(split_string) == 1:
+        if cut_last:
+            return split_string[0][:-1]
+
+    words = split_string[:-1]
+    proc_words = [word[:-1] for word in words]
+    if cut_last:
+        proc_words.append(split_string[-1][:-1])
+    else:
+        proc_words.append(split_string[-1])
+
+    return ''.join(proc_words)
+
+
 def backspace_compare(first: str, second: str):
-    ...
+
+    first_result = process_string(first)
+    print(first_result)
+    second_result = process_string(second)
+    print(second_result)
+
+    return first_result == second_result
+
+
+if __name__ == '__main__':
+    s = "ab#c"
+    t = "ad#c"
+    print(backspace_compare(s, t), end='\n\n')
+
+    s = "a##c"
+    t = "#a#c"
+    print(backspace_compare(s, t), end='\n\n')
+
+    s = "a##casd#"
+    t = "#a#casd#"
+    print(backspace_compare(s, t), end='\n\n')
+
+    s = "a#c"
+    t = "b"
+    print(backspace_compare(s, t))
--- homework7/hw3.py
+++ homework7/hw3.py
@@ -21,6 +21,118 @@ Example:
 """
 from typing import List
 
+# Sentinel object
+empty_element = object()
+
+
+class EmptyLineError(Exception):
+    """Handle exceptions related to board values"""
+
+
+class LineState:
+    """Store states of one line from board"""
+    def __init__(self, x_win: bool, y_win: bool, unfinished: bool) -> None:
+        self.x_win = x_win
+        self.y_win = y_win
+        self.unfinished = unfinished
+
+
+def check_line(line_type: str, board: List[List], index: int = 0) -> LineState:
+    """Collect states from one line on board"""
+    list_to_check = get_line(line_type, board, index)
+    x_win = all([cell == 'x' for cell in list_to_check])
+    y_win = all([cell == 'o' for cell in list_to_check])
+    unfinished = bool('-' in list_to_check)
+
+    state = LineState(x_win, y_win, unfinished)
+    return state
+
+
+def get_line(line_type: str, board: List[List], index: int = 0) -> List[str]:
+    """Define elements from board for current line"""
+    list_to_check = empty_element  # sentinel
+
+    if line_type == 'horizontal':
+        list_to_check = board[index]
+
+    if line_type == 'vertical':
+        list_to_check = [cell[index] for cell in board]
+
+    # From top left to bottom right
+    if line_type == 'positive_diagonal':
+        list_to_check = [board[i][i] for i in range(3)]
+
+    # From bottom left to top right
+    if line_type == 'negative_diagonal':
+        list_to_check = [board[i][2-i] for i in range(3)]
+
+    if list_to_check == empty_element:
+        raise EmptyLineError('Board line is empty')
+
+    return list_to_check
+
+
+def check_states(states: List[LineState]) -> str:
+    """Check conditions for all possible game states"""
+    if any([state.x_win for state in states]):
+        return 'x wins!'
+
+    if any([state.y_win for state in states]):
+        return 'o wins!'
+
+    if any([state.unfinished for state in states]):
+        return 'unfinished!'
+
+    return 'draw!'
+
+
+def get_states(board: List[List]) -> List[LineState]:
+    """Process all possible game states"""
+    states = list()
+    states.append(check_line('positive_diagonal', board))
+    states.append(check_line('negative_diagonal', board))
+
+    for line_index in range(3):
+        states.append(check_line('horizontal', board, line_index))
+        states.append(check_line('vertical', board, line_index))
+
+    return states
+
 
 def tic_tac_toe_checker(board: List[List]) -> str:
-    ...
+    """Check current game state"""
+    board_states = get_states(board)
+    result = check_states(board_states)
+    return result
+
+
+if __name__ == '__main__':
+    test_board = [['-', '-', 'o'],
+                  ['-', 'o', 'o'],
+                  ['x', 'x', 'x']]
+
+    print(tic_tac_toe_checker(test_board))
+
+    test_board = [['-', '-', 'o'],
+                  ['-', 'x', 'o'],
+                  ['x', 'o', 'x']]
+
+    print(tic_tac_toe_checker(test_board))
+
+    test_board = [['-', '-', 'x'],
+                  ['-', 'x', 'o'],
+                  ['x', 'o', 'x']]
+
+    print(tic_tac_toe_checker(test_board))
+
+    test_board = [['-', '-', 'x'],
+                  ['o', 'o', 'o'],
+                  ['x', 'o', 'x']]
+
+    print(tic_tac_toe_checker(test_board))
+
+    test_board = [['o', 'x', 'o'],
+                  ['o', 'x', 'o'],
+                  ['x', 'o', 'x']]
+
+    print(tic_tac_toe_checker(test_board))
