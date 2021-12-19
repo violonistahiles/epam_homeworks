f5ba4b45a801f75d0665540476ae3af5e529b818
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-12-19 13:01:02 +0300

    Task02: Add function and class and its tests

--- homework9/hw2.py
+++ homework9/hw2.py
@@ -6,3 +6,37 @@ Do it both ways: as a class and as a generator.
 ...    [][2]
 
 """
+from contextlib import contextmanager
+
+
+@contextmanager
+def simple_context_manager(exception: Exception):
+    """
+    Context manager function witch suppresses passed exception
+
+    :param exception: Exception type to suppresses
+    :type exception: Exception
+    """
+    try:
+        yield
+    except exception:
+        pass
+
+
+class SimpleContextManager:
+    """
+    Context manager class witch suppresses passed exception
+    """
+    def __init__(self, exception: Exception):
+        """
+        :param exception: Exception type to suppresses
+        :type exception: Exception
+        """
+        self._exception_to_ignore = exception
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self._exception_to_ignore == exc_type:
+            return True
