2b07e9ff8c5267545b9a5c17fff3c81acf750ee8
Grigoriy Kurushin <violonistahiles8@gmail.com>
2021-12-31 00:50:00 +0300

    task02: Add solution

---
+++ tests/homework11_tests/test_hw2.py
@@ -0,0 +1,20 @@
+from homework11.hw2 import Order, elder_discount, morning_discount
+
+
+def test_morning_discount():
+    """Testing morning discount works correctly"""
+    assert morning_discount(100) == 75
+
+
+def test_elder_discount():
+    """Testing elder discount works correctly"""
+    assert elder_discount(100) == 10
+
+
+def test_order_strategy_setting():
+    """Testing that strategy setting in Order class works correctly"""
+    order_1 = Order(100, morning_discount)
+    assert order_1.final_price() == 75
+
+    order_2 = Order(100, elder_discount)
+    assert order_2.final_price() == 10
