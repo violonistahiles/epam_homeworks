7a547f0ba609cf2db2fe52b528c1df38c1dca1a0
Grigoriy Kurushin <violonistahiles8@gmail.com>
2022-01-08 14:35:36 +0300

    Task02: Make all class attributes definition in __init__ method

--- homework8/task02.py
+++ homework8/task02.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from typing import Callable
+from typing import Callable, Dict, List
 
 import toml
 
@@ -47,15 +47,16 @@ class TableData:
         :type table_name: str
         """
         current_path = os.path.abspath(os.getcwd())
-        self._cursor = None
         self._dir_path = os.path.join(current_path, 'homework8')
+        self._iter_value = empty_element
+        self._cursor = None
         self._database_name = database_name
 
-        self._read_commands(table_name)
+        self._commands = self._read_commands(table_name)
         self._check_table(table_name)
 
     @prepare_database
-    def _check_table(self, table_name):
+    def _check_table(self, table_name: str):
         """
         Check if database contain table with specified name
 
@@ -67,63 +68,73 @@ class TableData:
         if table_name not in tables:
             raise TableNotExists
 
-    def _read_commands(self, table_name):
+    def _read_commands(self, table_name: str) -> Dict:
         """
         Set up command for interaction with database
 
         :param table_name: Table name from database for interaction
         :type table_name: str
+        :return: Dictionary with commands related to table_name
+        :rtype: Dict
         """
         commands_file = os.path.join(self._dir_path, 'cmd.toml')
         with open(commands_file) as fi:
-            self._commands = toml.load(fi)
+            commands = toml.load(fi)
 
-        for act in self._commands:
-            self._commands[act] = self._commands[act].format(table_name)
+        for act in commands:
+            commands[act] = commands[act].format(table_name)
 
-    def _read_table_columns(self):
+        return commands
+
+    def _read_table_columns(self) -> List:
         """
         Read columns from selected table in database
+
+        :return: List with names of columns from table
+        :rtype: List
         """
         data = self._cursor.execute(self._commands['COLUMNS'])
-        self._columns = [column[0] for column in data.description]
+        columns = [column[0] for column in data.description]
+        return columns
 
     @prepare_database
-    def __len__(self):
+    def __len__(self) -> int:
         db_response = self._cursor.execute(self._commands['LEN'])
         table_len = db_response.fetchall()[0][0]
         return table_len
 
     @prepare_database
-    def __getitem__(self, item):
-        self._read_table_columns()  # Update columns if table was changed
+    def __getitem__(self, item: str):
+        # Update columns if table was changed
+        columns = self._read_table_columns()
         sample = self._cursor.execute(self._commands['GETITEM'],
                                       {'name': item})
         result = sample.fetchall()
         if result:
-            return {key: value for key, value in zip(self._columns, result[0])}
+            return {key: value for key, value in zip(columns, result[0])}
 
-    def __contains__(self, item):
+    def __contains__(self, item: str):
         result = self.__getitem__(item)
         return True if result else False
 
     def __iter__(self):
         # Set flag for starting iteration from first sorted element from table
-        self.iter_value = empty_element
+        self._iter_value = empty_element
         return self
 
     @prepare_database
     def __next__(self):
-        self._read_table_columns()  # Update columns if table was changed
-        if self.iter_value == empty_element:
+        # Update columns if table was changed
+        columns = self._read_table_columns()
+        if self._iter_value == empty_element:
             sample = self._cursor.execute(self._commands['ITER_START'])
         else:
             sample = self._cursor.execute(self._commands['ITER_NEXT'],
-                                          {'name': self.iter_value})
+                                          {'name': self._iter_value})
         result = sample.fetchall()
         if result:
-            elem = {key: value for key, value in zip(self._columns, result[0])}
-            self.iter_value = elem['name']
+            elem = {key: value for key, value in zip(columns, result[0])}
+            self._iter_value = elem['name']
             return elem
 
         raise StopIteration
